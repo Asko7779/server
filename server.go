c9e5d7cfc47a653351bc3fd273b557dc953c2d89
Asko7779 <169293034+Asko7779@users.noreply.github.com>
2025-01-18 13:43:00 +0100

    Add files via upload

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		os.Exit(1)
+	}
+	defer ln.Close()
+
+	fmt.Println("HTTP Server started on 127.0.0.1:8080")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+		fmt.Println("[Connected] from", conn.RemoteAddr())
+		response := "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nTesting Server"
+		conn.Write([]byte(response))
+		conn.Close()
+	}
+}
